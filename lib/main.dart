0b8d9ead9d1ca0f3a14c53706e07f0aa85c576d9
Varun Resh Maddali <mvresh@gmail.com>
2019-10-21 16:21:02 +0530

    1)added Article page 2)Added Serif Font 3) Adjusted the detailed Article page to look similar to the one in course notes 4) deleted elevation on card

--- lib/main.dart
+++ lib/main.dart
@@ -72,31 +72,34 @@ class _NewsListPageState extends State<NewsListPage> {
             var article = articleList.articles[index];
             return GestureDetector(
               onTap: (){
-                _launchURL(url) async {
-
-                  if (await canLaunch(url)) {
-                    await launch(url,forceWebView: true);
-                  } else {
-                    throw 'Could not launch ${url}';
-                  }
-                }
-                _launchURL(article.url);
+//                _launchURL(url) async {
+//
+//                  if (await canLaunch(url)) {
+//                    await launch(url,forceWebView: true);
+//                  } else {
+//                    throw 'Could not launch ${url}';
+//                  }
+//                }
+//                _launchURL(article.url);
+                Navigator.push((context),
+                    MaterialPageRoute(builder: (context) => NewsDetailsPage(article)));
               },
               child: Card(
-                elevation: 2,
                 child: Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Text(article.title,
                           maxLines: 2,
                           textAlign: TextAlign.left,
-                          style: TextStyle(
+                          style: TextStyle(fontFamily: 'Noto',
                               fontWeight: FontWeight.bold, fontSize: 18)),
                       SizedBox(
                         height: 10,
                       ),
                       Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisSize: MainAxisSize.max,
                         children: <Widget>[
                           Expanded(
@@ -104,17 +107,15 @@ class _NewsListPageState extends State<NewsListPage> {
                             child: Text(
                               article.description,
                               maxLines: 4,
+                              style: TextStyle(fontFamily: 'Noto',fontSize: 15,)
                             ),
                           ),
-                          Flexible(
+                          Expanded(
                             flex: 1,
-                            child: Container(
-                              width: 100,
-                              child: Image(
-                                fit: BoxFit.fill,
-                                image: NetworkImage(
-                                    article.urlToImage),
-                              ),
+                            child: Image(
+                              fit: BoxFit.fitHeight,
+                              image: NetworkImage(
+                                  article.urlToImage),
                             ),
                           ),
                         ],
@@ -129,7 +130,7 @@ class _NewsListPageState extends State<NewsListPage> {
                             flex: 5,
                             child: Container(
                               alignment: Alignment.topLeft,
-                              child: Text('${article.source.name}',
+                              child: Text('${article.source.name}     ${DateTime.now().difference(DateTime.parse(article.publishedAt)).inHours} hours ago',
                                   style: TextStyle(color: Colors.grey)),
                             ),
                           ),
@@ -159,3 +160,109 @@ class _NewsListPageState extends State<NewsListPage> {
     );
   }
 }
+
+class NewsDetailsPage extends StatefulWidget {
+  final Articles article;
+  NewsDetailsPage(this.article);
+
+  @override
+  _NewsDetailsPageState createState() => _NewsDetailsPageState();
+}
+
+class _NewsDetailsPageState extends State<NewsDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'BBC News',
+          style: TextStyle(color: Colors.grey, fontSize: 25,fontFamily: 'Noto'),
+        ),
+        leading: IconButton(
+          onPressed: () {Navigator.pop(context);},
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.blueGrey,
+          ),
+        ),
+        actions: <Widget>[
+          Icon(
+            Icons.share,
+            color: Colors.blueGrey,
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Icon(
+            Icons.more_vert,
+            color: Colors.blueGrey,
+          ),
+          SizedBox(
+            width: 7,
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            //title
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(widget.article.title,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 35,fontFamily: 'Noto',fontWeight: FontWeight.bold,fontStyle: FontStyle.italic)),
+            ),
+//            description
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(widget.article.description,
+                  style: TextStyle(fontSize: 20,fontFamily: 'Noto')),
+              ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Image(
+                fit: BoxFit.fitHeight,
+                image: NetworkImage(
+                    widget.article.urlToImage),
+              )
+            ),
+//            image caption
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                style: TextStyle(
+                    color: Colors.grey,fontFamily: 'Noto'),
+              ),
+            ),
+//            source name
+            Padding(
+              padding: EdgeInsets.fromLTRB(10,10,0,5),
+              child: Text("By ${widget.article.source.name}",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold,fontSize: 18)),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10,10,0,5),
+              child: Text("${DateTime.parse(widget.article.publishedAt).day} - ${DateTime.parse(widget.article.publishedAt).month} - ${DateTime.parse(widget.article.publishedAt).year}",
+                  style: TextStyle(
+                      color: Colors.red,fontFamily: 'Noto',fontSize: 18)),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10,10,10,0),
+              child: Text("${widget.article.content}",
+                  style: TextStyle(fontSize: 20,
+                      color: Colors.black,fontFamily: 'Noto')),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
